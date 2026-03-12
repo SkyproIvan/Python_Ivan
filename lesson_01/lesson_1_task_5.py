e2df85174abc8616058a46eb835d2536672bae25
admin_one <ivan.nik.test.1988@gmail.com>
2026-03-12 21:08:29 +0300

    Исправлены недочеты в коде

--- lesson_01/lesson_1_task_5.py
+++ lesson_01/lesson_1_task_5.py
@@ -1,6 +1,7 @@
 def print_letter(num):
     print(num, end='')
 
+
 print_letter(8)
 print_letter(8)
 print_letter(0)
@@ -11,4 +12,4 @@ print_letter(5)
 print_letter(3)
 print_letter(5)
 print_letter(3)
-print_letter(5) 
+print_letter(5)
